c11114b4219d675755ff4ebbfd2e2b58806eeede
wlp <w703001848@qq.com>
2022-04-14 18:17:32 +0800

    modify node.js express

--- node.js/code/main.js
+++ node.js/code/main.js
@@ -1,12 +1,24 @@
 // 导入 express 模块
 const express = require('express')
+const cors = require('cors')
 const apiRouter = require('./router/index.js')
 
 // 创建 web 服务器
 const app = express()
 
-// CORS 跨域设置
-app.all('*', function (req, res, next) {
+// 设置静态资源目录
+app.use(express.static('files'))
+app.use('/img', express.static('static'))
+
+// 挂载时间戳中间件
+app.use(function (req, res, next) {
+    const t = parseInt(new Date().getTime()/1000)
+    req.timestamp = t
+    next()
+})
+
+// jsonp 跨域设置(用于支持老版本浏览器)
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild')
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
@@ -18,21 +30,24 @@ app.all('*', function (req, res, next) {
     }
 })
 
-// 设置静态资源目录
-app.use(express.static('files'))
-app.use('/img', express.static('static'))
+// 设置路由(支持老版本浏览器接口)
+app.get('/api/jsonp/index', (req, res) => {
+    const query = req.query
+    res.send({
+        code: 200,
+        data: query,
+        msg: 'success',
+        timestamp: req.timestamp,
+    })
+})
+
+// CORS 跨域设置
+app.use(cors())
 
 // 配置解析 JSON 格式 POST 请求的中间件
 app.use(express.json())
 // 配置解析表单 POST 请求的中间件
 app.use(express.urlencoded({ extended: true }))
-
-// 挂载时间戳中间件
-app.use(function (req, res, next) {
-    const t = parseInt(new Date().getTime()/1000)
-    req.timestamp = t
-    next()
-})
 
 // 设置路由
 app.use('/api', apiRouter)
